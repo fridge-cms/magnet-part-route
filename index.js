ef0ad51d7e7ada810694ed9a30db9406744a4c09
Mike Kruk <mike@ripeworks.com>
2019-05-20 20:43:45 -0400

    support rendering as link

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const Route = ({ pattern, content }) => (
-  <span>
-    {pattern.replace(/{(\w*)}/g, (m, key) =>
-      content.hasOwnProperty(key) ? content[key] : ""
-    )}
-  </span>
-);
+const Route = ({ pattern, content, asLink = false }) => {
+  const url = pattern.replace(/{(\w*)}/g, (m, key) =>
+    content.hasOwnProperty(key) ? content[key] : ""
+  );
+
+  if (asLink) {
+    return (
+      <a href={url} target="_blank">
+        {url}
+      </a>
+    );
+  }
+
+  return <span>{url}</span>;
+};
 
 const part = {
   name: "Route",
@@ -13,14 +21,20 @@ const part = {
     pattern: {
       label: "URL Pattern",
       type: "string"
+    },
+    link: {
+      label: "Display as link?",
+      type: "boolean"
     }
   },
   schema: part => ({
     type: "string",
     component: ({ config: { content } }) => (
-      <Route pattern={part.pattern} content={content} />
+      <Route pattern={part.pattern} content={content} asLink={part.link} />
     ),
-    render: ({ content }) => <Route pattern={part.pattern} content={content} />
+    render: ({ content }) => (
+      <Route pattern={part.pattern} content={content} asLink={part.link} />
+    )
   })
 };
 
